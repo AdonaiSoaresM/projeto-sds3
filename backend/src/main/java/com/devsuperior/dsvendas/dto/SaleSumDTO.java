433dedd7007f3b1e53d0c66520dd733ac058fa39
Adonai Soares <adonaielite@gmail.com>
2021-11-07 22:10:37 -0300

    Primeira requisicao

--- backend/src/main/java/com/devsuperior/dsvendas/dto/SaleSumDTO.java
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SaleSumDTO.java
@@ -17,11 +17,11 @@ public class SaleSumDTO implements Serializable{
 		this.sum = sum;
 	}
 
-	public String getSallerName() {
+	public String getSellerName() {
 		return sellerName;
 	}
 
-	public void setSallerName(String sellerName) {
+	public void setSellerName(String sellerName) {
 		this.sellerName = sellerName;
 	}
 
