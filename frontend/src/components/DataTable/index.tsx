11708499a36e76cbedcb94b7361a73a69bb9fdf9
Adonai Soares <adonaielite@gmail.com>
2022-01-13 12:12:45 -0300

    Error

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -7,8 +7,8 @@ import { BASE_URL } from "utils/requests";
 
 function toMoeda(valor: number){
     let valorConvertido = (parseFloat(valor.toFixed(2)).toLocaleString('pt-BR'))
-    let valorFinal = valorConvertido.indexOf(',') == -1 ? valorConvertido.concat(',00') : valorConvertido
-    valorFinal = (valorFinal.length - (valorFinal.indexOf(',') + 1)) == 2 ? valorFinal : valorFinal.concat('0')
+    let valorFinal = valorConvertido.indexOf(',') === -1 ? valorConvertido.concat(',00') : valorConvertido
+    valorFinal = (valorFinal.length - (valorFinal.indexOf(',') + 1)) === 2 ? valorFinal : valorFinal.concat('0')
     return valorFinal }
 
 function DataTable() {
